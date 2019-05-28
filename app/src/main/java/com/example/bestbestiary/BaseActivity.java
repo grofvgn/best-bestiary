8ec684d723d343eebdf70e15beb94d8cb208b5e4
grofvgn <dmitry.leposha@gmail.com>
2019-05-28 17:12:54 +0200

    Dodano: XML sablona za vsako aktivnost, BaseActivity ter IntroActivity.

---
+++ app/src/main/java/com/example/bestbestiary/BaseActivity.java
@@ -0,0 +1,6 @@
+package com.example.bestbestiary;
+import android.app.Activity;
+
+public class BaseActivity extends Activity {
+    public static final String GAME_PREFERENCES = "GamePrefs";
+}
---
+++ app/src/main/java/com/example/bestbestiary/IntroActivity.java
@@ -0,0 +1,13 @@
+package com.example.bestbestiary;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class IntroActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_intro);
+    }
+}
